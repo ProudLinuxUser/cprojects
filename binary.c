9abafc48e4162758144b841df0e257ffd411f6c9
Mathias <222339287+ProudLinuxUser@users.noreply.github.com>
2026-02-11 20:04:11 +0100

    Update binary.c

--- binary.c
+++ binary.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 void dec_bin(int decimal);
+void ftext_bin(char askusr);
 
 int main(void)
 {
         char askusr[100];
-        printf("Decimel to Binary converter\n");
+        printf("Decimal to Binary converter\n");
         printf("---------------------------\n");
         printf("Enter Ascii character to convert into Decimal and Binary: ");
         scanf("%99s",askusr);
@@ -15,6 +16,16 @@ int main(void)
                 int value = (int)askusr[i];
                 printf("%c -> %d -> ",askusr[i],value);
                 dec_bin(value);
+        }
+
+        printf("---------------------------\n");
+        printf("Full word into Binary: \n");
+        printf("%s:",askusr);
+        for (int j = 0; askusr[j] != '\0' ;j++)
+        {
+                int ffull = (int)askusr[j];
+                printf(">");
+                ftext_bin(ffull);
         }
 }
 
@@ -34,4 +45,20 @@ void dec_bin(int decimal)
                 printf("%i",bits[j]);
         }
         printf("\n");
+}
+
+void ftext_bin(char full)
+{
+        int fbits[8];
+
+        for (int i = 7; i >= 0; i--)
+        {
+                fbits[i] = full % 2;
+                full /= 2;
+        }
+
+        for (int j = 0; j < 8; j++)
+        {
+                printf("%i",fbits[j]);
+        }
 }
